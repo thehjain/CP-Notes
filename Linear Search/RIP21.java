f768e8a0968b785f3ce35cddf6b4e2c38c978401
Harsh <thehjain@gmail.com>
2021-01-17 11:19:33 +0530

    Rest In Peace - 21-1!

---
+++ Linear Search/RIP21.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class RIP21 {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		String s = sc.next();
+
+		int num = Integer.parseInt(s);
+
+		if (s.indexOf("21") > -1 || num % 21 == 0)
+			System.out.println("The streak is broken!");
+		else
+			System.out.println("The streak lives still in our heart!");
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
