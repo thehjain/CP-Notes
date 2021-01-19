3e32c0ee6e0d656b338883616ce23e56b6faa425
Harsh <thehjain@gmail.com>
2021-01-19 11:08:20 +0530

    Transmutation Diamonds

---
+++ Prefix Sum/MXPOWER.java
@@ -0,0 +1,95 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class MXPOWER {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+
+		int[][] arr = new int[n][n];
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				arr[i][j] = sc.nextInt();
+
+		int res = MIN;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				int sum = arr[i][j];
+				res = Math.max(sum, res);
+
+				for (int k = 1; i + k < n && j + k < n && i - k >= 0 && j - k >= 0; k++) {
+
+					sum += arr[i][j - k];
+					sum += arr[i][j + k];
+					sum += arr[i - k][j];
+					sum += arr[i + k][j];
+
+					for (int l = 1; l < k; l++) {
+
+						sum += arr[i - l][j - k + l];
+						sum += arr[i + l][j - k + l];
+						sum += arr[i - l][j + k - l];
+						sum += arr[i + l][j + k - l];
+
+					}
+					res = Math.max(sum, res);
+				}
+
+			}
+		}
+		System.out.println(res);
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
