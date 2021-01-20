1b56e5e3e2b0499a4ad167744eb2d265ecfed7ca
Harsh <thehjain@gmail.com>
2021-01-20 11:34:52 +0530

    Pending

--- Prefix Sum/SimpleModification.java
+++ Prefix Sum/SimpleModification.java
@@ -10,15 +10,90 @@ class SimpleModification {
 	static FastScanner sc = new FastScanner();
 
 	public static void main(String[] args) throws IOException {
-		int T = sc.nextInt();
-		while (T-- > 0) {
-			solve();
-		}
+		// int T = sc.nextInt();
+		// while (T-- > 0) {
+		solve();
+		// }
 	}
 
 	static void solve() throws IOException {
 
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 
+		int modifications = sc.nextInt();
+		int queries = sc.nextInt();
+
+		int[][] arr = new int[n][m];
+		int[][] temp = new int[n][m];
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				arr[i][j] = sc.nextInt();
+
+		for (int i = 0; i < modifications; i++) {
+
+			int k = sc.nextInt();
+
+			int r1 = sc.nextInt();
+			int c1 = sc.nextInt();
+			int r2 = sc.nextInt();
+			int c2 = sc.nextInt();
+
+			temp[r1][c1] += k;
+			if (r2 + 1 < n) {
+				temp[r2 + 1][c1] -= k;
+			}
+			if (c2 + 1 < m) {
+				temp[r1][c2 + 1] -= k;
+			}
+			if (r2 + 1 < n && c2 + 1 < m) {
+				temp[r2 + 1][c2 + 1] += k;
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (i != 0)
+				temp[i][0] = temp[i][0] + temp[i - 1][m - 1];
+			for (int j = 1; j < m; j++) {
+				temp[i][j] += temp[i][j - 1];
+			}
+		}
+
+		for (int[] row : temp) {
+			for (int ele : row)
+				System.out.print(ele + " ");
+			System.out.println();
+		}
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				arr[i][j] += temp[i][j];
+
+		for (int i = 0; i < n; i++) {
+			if (i != 0)
+				arr[i][0] = arr[i][0] + arr[i - 1][m - 1];
+			for (int j = 1; j < m; j++) {
+				arr[i][j] += arr[i][j - 1];
+			}
+		}
+
+		for (int[] row : arr) {
+			for (int ele : row)
+				System.out.print(ele + " ");
+			System.out.println();
+		}
+
+		for (int i = 0; i < queries; i++) {
+
+			int r1 = sc.nextInt();
+			int c1 = sc.nextInt();
+			int r2 = sc.nextInt();
+			int c2 = sc.nextInt();
+
+			System.out.println(arr[r2][c2] - arr[r1][c1]);
+
+		}
 
 	}
 
