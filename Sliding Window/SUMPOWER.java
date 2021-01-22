29ca76e6eeaf0550912eaf8ab903f2c61ca14f4a
Harsh <thehjain@gmail.com>
2021-01-22 21:28:41 +0530

    Summary Power

--- Sliding Window/SUMPOWER.java
+++ Sliding Window/SUMPOWER.java
@@ -8,18 +8,37 @@ class SUMPOWER {
 	private static int MIN = Integer.MIN_VALUE;
 	private static int MOD = 1000000007;
 	static FastScanner sc = new FastScanner();
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		int T = sc.nextInt();
 		while (T-- > 0) {
 			solve();
 		}
+		System.out.println(sb);
 	}
 
 	static void solve() throws IOException {
 
-		System.out.println("hello");
+		int n = sc.nextInt();
+		int k = sc.nextInt();
 
+		char[] arr = sc.next().toCharArray();
+
+		int count = 1;
+		long res = 0;
+
+		for (int i = 1; i < k; i++)
+			if (arr[i] != arr[i - 1]) count++;
+
+		for (int i = k; i < n; i++) {
+			res += count;
+			if (arr[i] == arr[i - 1]) res--;
+			else count++;
+			if (arr[i - k] != arr[i - k + 1]) count--;
+		}
+
+		sb.append(res + "\n");
 	}
 
 	static class FastScanner {
