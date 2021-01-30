5e11b70dfb6c8bd0d98b1adba73f19341e373c30
Harsh <thehjain@gmail.com>
2021-01-30 10:55:59 +0530

    Two Arrays and Swaps

---
+++ Constructive Problems/TwoArraysAndSwaps.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class TwoArraysAndSwaps {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		int[] arr = new int[n];
+		int[] brr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+
+		for (int i = 0; i < n; i++)
+			brr[i] = sc.nextInt();
+
+		Arrays.sort(arr);
+		Arrays.sort(brr);
+
+		long sum = 0;
+
+		for (int i = 0, j = n - 1; i < n; i++) {
+
+			if (arr[i] < brr[j] && k > 0) {
+				k--;
+				sum += brr[j--];
+			} else
+				sum += arr[i];
+
+		}
+
+		System.out.println(sum);
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
