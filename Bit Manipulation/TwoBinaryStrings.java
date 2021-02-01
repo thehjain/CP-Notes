45a87f0d039ce60e74449d27969c0b607588c7a5
Harsh <thehjain@gmail.com>
2021-02-01 22:36:49 +0530

    You Are Given Two Binary Strings

---
+++ Bit Manipulation/TwoBinaryStrings.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class TwoBinaryStrings {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+		System.out.print(sb);
+	}
+
+	static void solve() throws IOException {
+
+		String X = sc.next();
+		String Y = sc.next();
+
+		int offset = 0;
+
+		while (Y.charAt(Y.length() - 1 - offset) == '0') offset++;
+		int K = 0;
+		while (X.charAt(X.length() - 1 - offset - K) == '0') K++;
+
+		sb.append(K + "\n");
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
