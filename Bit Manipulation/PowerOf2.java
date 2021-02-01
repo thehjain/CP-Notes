2e0a7de0b34ce1cb8ad719b7994cddd9091fa3db
Harsh <thehjain@gmail.com>
2021-02-01 15:04:34 +0530

    Power of 2

---
+++ Bit Manipulation/PowerOf2.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class PowerOf2 {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		// int T = sc.nextInt();
+		// while (T-- > 0) {
+		solve();
+		// }
+	}
+
+	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+
+		int count = 0;
+		for (int i = 0; i < n; i++)
+			if (isPower(sc.nextLong())) count++;
+
+		System.out.print(count);
+
+	}
+
+	static boolean isPower(long a) {
+		return a != 0 && ((a & (a - 1)) == 0);
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
