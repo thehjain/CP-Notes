d376da01283da3813b47eb4b5dc2ef673be8637b
Harsh <thehjain@gmail.com>
2021-02-02 13:14:35 +0530

    K-Multiple Free Set

---
+++ Greedy Algorithms/KMultipleFreeSet.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class KMultipleFreeSet {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		// int T = sc.nextInt();
+		// while (T-- > 0) {
+		solve();
+		// }
+	}
+
+	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+
+		HashSet<Integer> set = new HashSet<>();
+
+		Arrays.sort(arr);
+
+		for (int ele : arr)
+			if (ele % k == 0 && set.contains(ele / k))
+				continue;
+			else
+				set.add(ele);
+
+		System.out.print(set.size());
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
