4f76d68bd73297aea466620cde709bd1e61a01fa
Harsh <thehjain@gmail.com>
2021-02-20 12:13:29 +0530

    Pending

--- String Algo's/LuckyCommonSubsequence.java
+++ String Algo's/LuckyCommonSubsequence.java
@@ -18,12 +18,63 @@ class LuckyCommonSubsequence {
 
 	static void solve() throws IOException {
 
-		String a = sc.next();
-		String b = sc.next();
+		char[] a = sc.next().toCharArray();
+		char[] b = sc.next().toCharArray();
 
-		String virus = sc.next();
+		char[] virus = sc.next().toCharArray();
+
+		int[][][] dp = new int[a.length + 1][b.length + 1][virus.length + 1];
+
+		for (int[][] grid : dp)
+			for (int[] row : grid)
+				Arrays.fill(row, -1);
+
+		int[] suffix = kmp(virus);
+
+		int sol = lps(a, b, virus, a.length - 1, b.length - 1, virus.length - 1, dp, suffix);
 
 
+
+	}
+
+	static int lps(char[] a, char[] b, char[] virus, int n, int m, int p, int[][][] dp, int[] suffix) {
+
+		if (p == -1)
+			return MAX;
+		if (n < 0 || m < 0)
+			return 0;
+		if (dp[n][m][p] != -1)
+			return dp[n][m][p];
+
+		int max = MIN;
+
+		if (a[n] == b[m]) {
+			int p2 = p;
+			while (p2 < virus.length - 1 && a[n] != virus[p2])
+				p2 = suffix[p2 + 1];
+			if (a[n] == virus[p1])
+				p2--;
+			max = 1 + lps(a, b, virus, n - 1, m - 1, p2, dp, suffix);
+		}
+
+		return dp[n][m][p] = Math.max(max, Math.max(lps(a, b, virus, n - 1, m, p, dp, suffix), lps(a, b, virus, n, m - 1, p, dp, suffix)));
+
+	}
+
+	static int[] kmp(char[] virus) {
+
+		int n = virus.length - 1;
+
+		int[] lps = new int[v.length];
+
+		lps[n] = n;
+
+		for (int i = v.length - 2; i >= 0; i--) {
+			while (n < v.length - 1 && lps[i] != lps[n])
+				n = lps[n + 1];
+			if (lps[i] == lps[n]) n--;
+			lps[i] = n;
+		}
 
 	}
 
